f158175770cad0c863e038434c2c5e92fca48dcc
HotakMuhib <123457don@gmail.com>
2024-07-02 11:31:01 -0400

    new change

--- src/main/java/com/cogent/api/SpringJenkinsApplication.java
+++ src/main/java/com/cogent/api/SpringJenkinsApplication.java
@@ -1,12 +1,27 @@
 package com.cogent.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class SpringJenkinsApplication {
-
+	
+	public static Logger Log = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		Log.info("Application started ......");
+	}
+	
 	public static void main(String[] args) {
+		
+		Log.info("Spring Project executed...");
+		Log.info("new change...");
+		
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
